82b8cf8eb4131a3d81010f04b86a7ad33c7ebeea
Naman Taneja <42579074+namantaneja167@users.noreply.github.com>
2021-10-05 13:42:48 +0530

    firebase-config update

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "@firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,6 +17,7 @@ import {getFirestore} from "@firebase/firestore";
 //   appId: process.env.APP_ID,
 //   measurementId: process.env.MEASUREMENT_ID,
 // };
+
 const firebaseConfig = {
     apiKey: "AIzaSyDydxSh4CHazi7ECET6c8Iww5L3EYqIYIw",
     authDomain: "firestore-tutorial-f0824.firebaseapp.com",
