d9678bcfd26203b5d283947d5d24e2e0993e5592
Naman Taneja <42579074+namantaneja167@users.noreply.github.com>
2021-10-05 13:27:37 +0530

    permission updated

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,14 +6,23 @@ import {getFirestore} from "@firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//   appId: process.env.APP_ID,
+//   measurementId: process.env.MEASUREMENT_ID,
+// };
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+    apiKey: "AIzaSyDydxSh4CHazi7ECET6c8Iww5L3EYqIYIw",
+    authDomain: "firestore-tutorial-f0824.firebaseapp.com",
+    projectId: "firestore-tutorial-f0824",
+    storageBucket: "firestore-tutorial-f0824.appspot.com",
+    messagingSenderId: "700177863980",
+    appId: "1:700177863980:web:41d3e6e52794cb468a0889",
+    measurementId: "G-YC8E81P2NY"
 };
 
 // Initialize Firebase
